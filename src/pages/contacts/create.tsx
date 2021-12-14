79c75a9315a96d15d0a2a3aa3465866359eecd62
Cassia Maria <cassiamariaassis@gmail.com>
2021-12-14 03:56:22 -0300

    Validação dos formulários

--- src/pages/contacts/create.tsx
+++ src/pages/contacts/create.tsx
@@ -1,8 +1,53 @@
-import { Box, Divider, Flex, Heading, Text, VStack, SimpleGrid, HStack, Button } from "@chakra-ui/react";
+import { 
+  Box, 
+  Divider, 
+  Flex, 
+  Heading, 
+  Text, VStack, 
+  SimpleGrid, 
+  HStack, 
+  Button 
+} from "@chakra-ui/react";
 import Link from "next/link";
+import { SubmitHandler, useForm } from 'react-hook-form'
 import { Input } from "../../components/Form/Input";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+
+type CreateContactData = {
+  id: number;
+  name: string;
+  phone: number;
+  email: string;
+  Uf: string;
+  street: string;
+  district: string;
+  city: string;
+  cep: number;
+};
+
+const createContactFormSchema = yup.object().shape({
+  name: yup.string().required('*Nome obrigatório'),
+  email: yup.string().required('*E-mail obrigatório').email('*E-mail inválido'),
+  campoObrigatorio: yup.string().required('*Campo obrigatório'),
+  // phone: yup.number().required(),
+  // cep: yup.number().required()
+
+})
 
 export default function CreateContact() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(createContactFormSchema)
+  });
+
+  const {errors} = formState;
+
+  const handleCreateUser: SubmitHandler<CreateContactData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    console.log(values);
+  }
+
   return (
     <Box>
       <Flex
@@ -27,28 +72,73 @@ export default function CreateContact() {
       
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
 
-        <Box flex="1" borderRadius={8} bg="gray.800" p={["6", "8"]}>
+        <Box 
+          as="form"
+          flex="1" 
+          borderRadius={8} 
+          bg="gray.800" 
+          p={["6", "8"]}
+          onSubmit={handleSubmit(handleCreateUser)}
+        >
           <Heading size="lg" fontWeight="normal">Criar novo contato</Heading>
           <Divider my="6" borderColor="gray.700" />
 
           <VStack spacing="8">
-            <Input name="name" label="Nome Completo" />
+            <Input 
+              name="name"
+              label="Nome Completo" 
+              {...register('name')}
+              error={errors.name}
+            />
 
             <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-              <Input name="email" label="E-mail" />
-              <Input name="phone" label="Número de telefone" />
+              <Input 
+                name="email" 
+                label="E-mail" 
+                {...register('email')}
+                error={errors.email}
+              />
+              <Input 
+                name="phone" 
+                label="Número de telefone" 
+                {...register('phone')} 
+              />
             </SimpleGrid>
 
             <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-              <Input name="cep" label="CEP" />
-              <Input name="uf" label="UF" />
+              <Input 
+                name="cep" 
+                label="CEP" 
+                {...register('cep')}
+              />
+              <Input
+                name="uf" 
+                label="UF" 
+                {...register('uf')}
+                error={errors.campoObrigatorio}
+              />
             </SimpleGrid>
 
-            <Input name="street" label="Logradouro" />
+            <Input 
+              name="street" 
+              label="Logradouro" 
+              {...register('street')}
+              error={errors.campoObrigatorio}
+            />
 
             <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
-              <Input name="district" label="Bairro" />
-              <Input name="city" label="Cidade" />
+              <Input 
+                name="district" 
+                label="Bairro" 
+                {...register('district')}
+                error={errors.campoObrigatorio}
+              />
+              <Input 
+                name="city" 
+                label="Cidade" 
+                {...register('city')}
+                error={errors.campoObrigatorio}
+              />
             </SimpleGrid>
           </VStack>
 
@@ -57,7 +147,11 @@ export default function CreateContact() {
               <Link href="/" passHref>
                 <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
               </Link>
-              <Button colorScheme="cyan">Salvar</Button>
+              <Button 
+                type="submit"
+                colorScheme="cyan"
+                isLoading={formState.isSubmitting}
+              >Salvar</Button>
             </HStack>
           </Flex>
         </Box>
